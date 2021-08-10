37d46b7d6aa3604c364cd01b31964cdeb2023202
Aravinth Raaj <aravinthraajk@gmail.com>
2021-08-10 13:56:35 +0200

    Code update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect ,useCallback} from 'react';
-import TodoForm from './Components/TodoForm';
-import TodoList from './Components/TodoList';
-import Todo from './Components/Todo';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react";
+import TodoForm from "./Components/TodoForm";
+import TodoList from "./Components/TodoList";
+import Todo from "./Components/Todo";
+import "./App.css";
 
-const LOCAL_STORAGE_KEY = 'todo-list-store';
+const LOCAL_STORAGE_KEY = "todo-list-store";
 
 export const todoContext = React.createContext({
   todo: {
-    id: '',
-    task: '',
+    id: "",
+    task: "",
     completed: false,
     comment: false,
-    comment_data: '',
+    comment_data: "",
   },
   setTodo: () => {},
 });
@@ -24,20 +24,19 @@ export const todosContext = React.createContext({
 
 function App() {
   const [todo, setTodo] = useState({
-    id: '',
-    task: '',
+    id: "",
+    task: "",
     completed: false,
     comment: false,
-    comment_data: '',
+    comment_data: "",
   });
   const [todos, setTodos] = useState([]);
-  // const [comments, setComments] =useState([]);
 
   useEffect(() => {
     const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
     if (storageTodos) {
       setTodos(storageTodos);
-      console.log(storageTodos);
+      // console.log(storageTodos);
     }
   }, []);
 
@@ -45,17 +44,17 @@ function App() {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
   }, [todos]);
 
-  const toggle = useCallback((name,id)=>{
-    setTodos(todos=>
+  const toggle = useCallback((name, id) => {
+    setTodos((todos) =>
       todos.map((todo) => {
         if (todo.id === id) {
-          if (name === 'completed') {
+          if (name === "completed") {
             return {
               ...todo,
               completed: !todo.completed,
             };
           }
-          if (name === 'comment') {
+          if (name === "comment") {
             return {
               ...todo,
               comment: !todo.comment,
@@ -65,29 +64,7 @@ function App() {
         return todo;
       })
     );
-  },[])
-  // function toggle(name, id) {
-  //   setTodos(todos=>
-  //     todos.map((todo) => {
-  //       if (todo.id === id) {
-  //         if (name === 'completed') {
-  //           return {
-  //             ...todo,
-  //             completed: !todo.completed,
-  //           };
-  //         }
-  //         if (name === 'comment') {
-  //           return {
-  //             ...todo,
-  //             comment: !todo.comment,
-  //           };
-  //         }
-  //       }
-  //       return todo;
-  //     })
-  //   );
-  // }
-  
+  }, []);
 
   function removeTodo(id) {
     setTodos(todos.filter((todo) => todo.id !== id));
@@ -96,6 +73,37 @@ function App() {
   function addTodo(todo) {
     setTodos((todos) => [todo, ...todos]);
   }
+  // console.log(todos);
+
+  // const apiresponse = async function get() {
+  //   const response = await fetch(
+  //     "https://stormy-spire-33682.herokuapp.com/v1/todo"
+  //   );
+  //   const data = await response.json();
+  //   return data;
+  // };
+  // function returndata(result){
+  //   console.log(result.data, " <- your data")
+  //   return result.data
+  // }
+  // const mydata = apiresponse();
+  // console.log(mydata);
+  // const HERE_WILL_BE_A_PROMISE_STILL = mydata.then((result) => returndata(result));
+  // console.log(HERE_WILL_BE_A_PROMISE_STILL);
+
+  // const apiresponse = async function get() {
+  //   const response = await fetch(
+  //     "https://stormy-spire-33682.herokuapp.com/v1/todo"
+  //   );
+  //   const data = await response.json();
+  //   return data;
+  // };
+  // function returndata(result){
+  //   return result.data
+  // }
+  // const mydata = apiresponse();
+  // const data = mydata.then((result) => returndata(result));
+  // console.log(data);
 
   return (
     <div className="App">
